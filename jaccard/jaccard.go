ba1e33e7561adf76c32284716804483b8650ae23
Hubert Bryłkowski <hubert.brylkowski@brainly.com>
2018-03-11 18:01:04 +0100

    simple intersection

--- jaccard/jaccard.go
+++ jaccard/jaccard.go
@@ -9,11 +9,29 @@ func NewStringSet() StringSet {
 	return StringSet{m}
 }
 
-func (s *StringSet) add(another string) {
+func (s *StringSet) Add(another string) {
 	s.set[another] = true
 }
 
-func (s StringSet) contains(another string) bool {
+func (s StringSet) Contains(another string) bool {
 	_, found := s.set[another]
 	return found
+}
+
+func (s StringSet) Elements() []string {
+	var keys []string
+	for key := range s.set {
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (s StringSet) Intersection(another StringSet) StringSet {
+	intersection := NewStringSet()
+	for _, element := range s.Elements() {
+		if another.Contains(element) {
+			intersection.Add(element)
+		}
+	}
+	return intersection
 }
--- jaccard/jaccard_test.go
+++ jaccard/jaccard_test.go
@@ -1,14 +1,45 @@
 package jaccard
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStringSetAdd(t *testing.T) {
 	stringSet := NewStringSet()
-	if stringSet.contains("something") {
+	if stringSet.Contains("something") {
 		t.Fail()
 	}
-	stringSet.add("something")
-	if !stringSet.contains("something") {
+	stringSet.Add("something")
+	if !stringSet.Contains("something") {
+		t.Fail()
+	}
+}
+
+func TestStringSetElements(t *testing.T) {
+	stringSet := NewStringSet()
+	stringSet.Add("1")
+	stringSet.Add("2")
+	elements := stringSet.Elements()
+	if len(elements) != 2 {
+		t.Fail()
+	}
+	if !((elements[0] == "1" && elements[1] == "2") || (elements[0] == "2" && elements[1] == "1")) {
+		t.Fail()
+	}
+}
+
+func TestStringSetIntersection(t *testing.T) {
+	stringSet := NewStringSet()
+	stringSet.Add("1")
+	stringSet.Add("2")
+	anotherSet := NewStringSet()
+	anotherSet.Add("3")
+	anotherSet.Add("2")
+	intersection := stringSet.Intersection(anotherSet)
+	if intersection.Elements()[0] != "2" {
+		t.Fail()
+	}
+	if len(intersection.Elements()) != 1 {
 		t.Fail()
 	}
 }
